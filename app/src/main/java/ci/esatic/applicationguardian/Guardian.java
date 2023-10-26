20b85ec3ef4f017561c24d3d8c2aecb7e12b1fee
iamSangohan <mensahluc81@gmail.com>
2023-10-26 11:36:19 +0000

    #feat = logout user and clear staticfiles

--- app/src/main/java/ci/esatic/applicationguardian/Guardian.java
+++ app/src/main/java/ci/esatic/applicationguardian/Guardian.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.textview.MaterialTextView;
 
@@ -29,6 +32,33 @@ public class Guardian extends AppCompatActivity {
             showUsername.setText(username);
         }
 
+        ImageButton registerVisiteur = findViewById(R.id.btn_register_visiteur);
+        ImageButton registerIncident = findViewById(R.id.btn_register_incident);
+        ImageButton scanQR = findViewById(R.id.btn_scan_qrcode);
+        ImageButton logout = findViewById(R.id.btn_logout);
 
+
+        // DECONNEXION
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deconnexion(); // Appeler la fonction de déconnexion
+            }
+        });
     }
+
+    private void deconnexion() {
+        // Supprimer les données d'authentification des SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("StaticsFile", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove("authToken");
+        editor.remove("userName");
+        editor.apply();
+
+        // Rediriger l'utilisateur vers l'écran de connexion (ou une autre page d'accueil)
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish(); // Terminer l'activité actuelle
+    }
+
 }
